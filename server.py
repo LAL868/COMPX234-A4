0f174cc7a24814b21d95829052d93e97cba4a834
LAL868 <3026146108@QQ.COM>
2025-06-14 14:19:34 +0000

    Resolve port number

--- server.py
+++ server.py
@@ -3,3 +3,9 @@ import threading
 import sys
 import base64
 import os
+
+if len(sys.argv) != 2:
+    print("Usage: python server.py <server_port>")
+    sys.exit(1)
+
+server_port = int(sys.argv[1])
