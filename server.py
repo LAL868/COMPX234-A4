cea1f21b5ed1e841ff16bb8239c2cb2e921b5c83
LAL868 <3026146108@QQ.COM>
2025-06-14 14:24:26 +0000

    create a primary UDP socket

--- server.py
+++ server.py
@@ -4,8 +4,14 @@ import sys
 import base64
 import os
 
+#resolve port number
 if len(sys.argv) != 2:
     print("Usage: python server.py <server_port>")
     sys.exit(1)
 
 server_port = int(sys.argv[1])
+
+# create a primary UDP socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(('0.0.0.0', server_port))  
+print(f"Server started on port {server_port}. Waiting for requests...")
